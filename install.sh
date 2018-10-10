ff1f80db3ad0b40ae2bc024d7a449abbe975646d
asdga78810 <43565847+asdga78810@users.noreply.github.com>
2018-10-11 01:27:03 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ sudo mv /usr/share/phpmyadmin/ /usr/share/phpMyAdmin/
 #Install Configure Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Ins$
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 #Install Configure phpMyAdmin
