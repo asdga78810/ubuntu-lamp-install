faa0e7a93e52b5aae1cc1ea552d933eb4c706ba9
asdga78810 <43565847+asdga78810@users.noreply.github.com>
2018-10-11 01:46:35 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y tzdata
 sudo ln -fs /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-sudo apt-get install nano nginx mariadb-server mariadb-client php-fpm php-mysql php-cli
+sudo apt-get install -y nano nginx mariadb-server mariadb-client php-fpm php-mysql php-cli
 
 ##Run mysql_secure_installation
 # Make sure that NOBODY can access the server without a password
@@ -32,7 +32,6 @@ echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
 #Configure nginx default
 sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
 sudo cp ~/default /etc/nginx/sites-available/default
-sudo mv /usr/share/phpmyadmin/ /usr/share/phpMyAdmin/
 #Install Configure Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
@@ -44,6 +43,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -yq install phpmyadmin
 sudo cp /usr/share/phpmyadmin/config.sample.inc.php /etc/phpmyadmin/config.inc.php
 sudo dpkg-reconfigure --frontend=noninteractive phpmyadmin
+sudo mv /usr/share/phpmyadmin/ /usr/share/phpMyAdmin/
 
 #Finish Restart Service
 sudo nginx -t
